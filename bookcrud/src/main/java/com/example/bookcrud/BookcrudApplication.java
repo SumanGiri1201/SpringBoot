e6dac0f23f102708bfcedcca56da744d030525a7
SumanGiri1201 <suman.giri.1201@gmail.com>
2025-06-29 21:22:59 +0530

    first commit

---
+++ bookcrud/src/main/java/com/example/bookcrud/BookcrudApplication.java
@@ -0,0 +1,12 @@
+
+package com.example.bookcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookcrudApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BookcrudApplication.class, args);
+	}
+}
---
+++ bookcrud/src/main/java/com/example/bookcrud/controller/BookController.java
@@ -0,0 +1,43 @@
+package com.example.bookcrud.controller;
+
+import com.example.bookcrud.model.Book;
+import com.example.bookcrud.repository.BookRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+@CrossOrigin
+public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable Long id, @RequestBody Book updatedBook) {
+        return bookRepository.findById(id)
+                .map(book -> {
+                    book.setTitle(updatedBook.getTitle());
+                    book.setAuthor(updatedBook.getAuthor());
+                    return bookRepository.save(book);
+                }).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+    }
+}
